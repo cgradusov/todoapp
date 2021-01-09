df4175bb76197818029dc6ee4591802157bf0b4a
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-09 19:51:33 +0300

    fix: change constant

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,10 @@ export const ADD_TODO_ITEM = 'ADD_TODO_ITEM';
 export const REMOVE_TODO_ITEM = 'REMOVE_TODO_ITEM';
 export const EDIT_TODO_ITEM = 'EDIT_TODO_ITEM';
 
-export const MARK_TODO_ITEM_DONE = 'EDIT_TODO_ITEM';
-export const MARK_TODO_ITEM_UNDONE = 'MARK_TODO_ITEM_UNDONE';
+export const TOGGLE_ITEM = 'TOGGLE_ITEM';
 
 export const FILTER_ALL = 'FILTER_ALL';
 export const FILTER_DONE = 'FILTER_DONE';
 export const FILTER_UNDONE = 'FILTER_UNDONE';
+
+export const INPUT_CHANGE = 'INPUT_CHANGE';
