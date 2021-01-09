c249a21ca1fa22f03da52336a4e95af507bc1fbc
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-09 16:50:07 +0300

    feat: add todo list filter reducer

--- src/reducers.js
+++ src/reducers.js
@@ -60,3 +60,16 @@ export const editTodoItem = (state = initialStateTodoItem, action = {}) => {
 const initialStateFilter = {
     filter: FILTER_ALL
 };
+
+export const changeFilter = (state = initialStateFilter, action = {}) {
+    switch (action.type) {
+        case FILTER_ALL:
+            return { ...state, filter: FILTER_ALL}
+        case FILTER_DONE:
+            return { ...state, filter: FILTER_DONE }
+        case FILTER_UNDONE:
+            return { ...state, filter: FILTER_UNDONE }
+        default:
+            return state       
+    }
+}
