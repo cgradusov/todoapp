a5d9e64ab3352ffac6d8c5499fe1e112a8e34d8f
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-11 16:04:23 +0300

    style: completely change TodoItem appearance

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,21 +1,44 @@
 import React from 'react'
+import { 
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+    ListItemIcon
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default function TodoItem({ text, done, index, onToggle, onRemove }) {
+    const [checked, setChecked] = React.useState(false);
+
     const pStyle = {
         textDecoration: done ? 'line-through' : 'none',
     }
 
-    const liStyle = {
-        display: 'flex',
-        gap: '10px',
-        alignItems: 'center'
+    const labelId = `checkbox-list-label-${index}`;
+    const onClick = () => {
+        onToggle()
+        setChecked(!checked)
     }
 
     return (
-        <li key={index} style={liStyle}>
-            <p style={pStyle} onClick={onToggle}>{text}</p>
-            <button onClick={onRemove}>[x]</button>
-        </li>  
+        <ListItem key={index} dense button onClick={onClick}>
+            <ListItemIcon>
+                <Checkbox
+                    checked={checked}
+                    edge="start"
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                />
+            </ListItemIcon>
+            <ListItemText style={pStyle}>{text}</ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton onClick={onRemove}>
+                    <DeleteIcon />
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>  
             
     )
 }
